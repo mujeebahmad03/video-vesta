156f014f33da81bb46ebb40447603cee611fba86
mujeebahmad <abdulmujeebahmad03@gmail.com>
2024-12-07 07:06:02 +0100

    implement social-auth

--- components/auth/social-auth.tsx
+++ components/auth/social-auth.tsx
@@ -1,91 +1,91 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { useSignIn, useSignUp } from "@clerk/nextjs";
+import { OAuthStrategy } from "@clerk/types";
 import { Github, Mail } from "lucide-react";
-import { useState } from "react";
-import { toast } from "sonner";
 
 import { Button } from "@/components/ui/button";
-import { ToastMessage } from "@/components/ui/toast-message";
 
 interface SocialSignInProps {
   type: "signUp" | "signIn";
 }
 
 export function SocialSignIn({ type }: SocialSignInProps) {
-  const [isLoading, setIsLoading] = useState<string | null>(null);
+  const { signIn } = useSignIn();
+  const { signUp, setActive } = useSignUp();
 
-  const handleSocialSignIn = async (provider: string) => {
-    setIsLoading(provider);
-    try {
-      // Here you would typically call your API to handle social sign-in
-      await new Promise((resolve) => setTimeout(resolve, 2000)); // Simulating API call
-      toast.success(
-        <ToastMessage
-          title={`${
-            type === "signUp" ? "Signed up" : "Signed in"
-          } with ${provider}`}
-          description="You have successfully authenticated."
-        />
-      );
-    } catch (error) {
-      console.log({ error });
-      toast.error(
-        <ToastMessage
-          title="Error"
-          description={`Failed to ${
-            type === "signUp" ? "sign up" : "sign in"
-          } with ${provider}. Please try again.`}
-        />
-      );
-    } finally {
-      setIsLoading(null);
-    }
+  if (!signIn || !signUp) return null;
+
+  const signInWith = (strategy: OAuthStrategy) => {
+    return signIn.authenticateWithRedirect({
+      strategy,
+      redirectUrl: "/sign-up/sso-callback",
+      redirectUrlComplete: "/dashboard",
+    });
   };
+
+  async function handleSignIn(strategy: OAuthStrategy) {
+    if (!signIn || !signUp) return null;
+
+    // If the user has an account in your application, but does not yet
+    // have an OAuth account connected to it, you can transfer the OAuth
+    // account to the existing user account.
+    const userExistsButNeedsToSignIn =
+      signUp.verifications.externalAccount.status === "transferable" &&
+      signUp.verifications.externalAccount.error?.code ===
+        "external_account_exists";
+
+    if (userExistsButNeedsToSignIn) {
+      const res = await signIn.create({ transfer: true });
+
+      if (res.status === "complete") {
+        setActive({
+          session: res.createdSessionId,
+        });
+      }
+    }
+
+    // If the user has an OAuth account but does not yet
+    // have an account in your app, you can create an account
+    // for them using the OAuth information.
+    const userNeedsToBeCreated =
+      signIn.firstFactorVerification.status === "transferable";
+
+    if (userNeedsToBeCreated) {
+      const res = await signUp.create({
+        transfer: true,
+      });
+
+      if (res.status === "complete") {
+        setActive({
+          session: res.createdSessionId,
+        });
+      }
+    } else {
+      // If the user has an account in your application
+      // and has an OAuth account connected to it, you can sign them in.
+      signInWith(strategy);
+    }
+  }
 
   return (
     <div className="space-y-3">
       <Button
         variant="outline"
         className="w-full"
-        onClick={() => handleSocialSignIn("Google")}
-        disabled={!!isLoading}
+        onClick={() => handleSignIn("oauth_google")}
       >
-        {isLoading === "Google" ? (
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-          >
-            {type === "signUp" ? "Signing up..." : "Signing in..."}
-          </motion.div>
-        ) : (
-          <>
-            <Mail className="mr-2 h-4 w-4" />
-            {type === "signUp" ? "Sign up" : "Sign in"} with Google
-          </>
-        )}
+        <Mail className="mr-2 h-4 w-4" />
+        {type === "signUp" ? "Sign up" : "Sign in"} with Google
       </Button>
+
       <Button
         variant="outline"
         className="w-full"
-        onClick={() => handleSocialSignIn("GitHub")}
-        disabled={!!isLoading}
+        onClick={() => handleSignIn("oauth_github")}
       >
-        {isLoading === "GitHub" ? (
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-          >
-            {type === "signUp" ? "Signing up..." : "Signing in..."}
-          </motion.div>
-        ) : (
-          <>
-            <Github className="mr-2 h-4 w-4" />
-            {type === "signUp" ? "Sign up" : "Sign in"} with GitHub
-          </>
-        )}
+        <Github className="mr-2 h-4 w-4" />
+        {type === "signUp" ? "Sign up" : "Sign in"} with GitHub
       </Button>
     </div>
   );
